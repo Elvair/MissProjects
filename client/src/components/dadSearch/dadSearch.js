a4ff43c3717dc3e17d29b5fa5b46183621c50d2b
Elvair <elvair@mountain>
2018-10-17 18:33:32 +0200

    Creando darSearch

---
+++ client/src/components/dadSearch/dadSearch.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import search from '../search/search';
+
+
+class dadSearch extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            author: "",
+            template: "",
+            search: ""
+        };
+
+    }
+    render() {
+        return (
+            <div className="Searches" >
+            </div>    
+
+
+    }
+}
+
+export default dadSearch;    
