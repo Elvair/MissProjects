39314116309095d8208f53aaf8349d57056ba96e
Elvair <elvair@mountain>
2018-10-18 18:19:29 +0200

    corrigiendo errores deploy

---
+++ server/public/precache-manifest.06f37d39a54bd5517897725592e62435.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "316f74eea106be2fcc2f",
+    "url": "/static/js/main.316f74ee.chunk.js"
+  },
+  {
+    "revision": "4c7333e9a6af7d2c4178",
+    "url": "/static/js/1.4c7333e9.chunk.js"
+  },
+  {
+    "revision": "316f74eea106be2fcc2f",
+    "url": "/static/css/main.ead35982.chunk.css"
+  },
+  {
+    "revision": "d8d806de3a132cd587f30329692693d9",
+    "url": "/index.html"
+  }
+];
