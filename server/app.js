c8d37628d75f96f2ef9eba992486362adfe2c08f
Elvair <elvair@mountain>
2018-10-18 16:36:35 +0200

    tras build para deploy en heroku

--- server/app.js
+++ server/app.js
@@ -80,4 +80,9 @@ app.use('/api/auth', authRouter);
 app.use('/projectTemplate',projectTemplateRouter);
 app.use('/search', searchRouter);
 
+app.use((req, res, next) => {
+  // If no routes match, send them the React HTML.
+   res.sendFile(__dirname + "/public/index.html");
+  });
+
 module.exports = app;
